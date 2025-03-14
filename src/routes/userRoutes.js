578a3ca0f22c27e4f37fa8f7f5702faa36d5d554
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-03-15 01:22:41 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { getAllUsers, getUserById, updateUser, deleteUser } = require('../controllers/userController');
+const { getAllUsers, getUserById, updateUser, deleteUser,getUserResults } = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getAllUsers); // Admin only
-router.get('/:id', authMiddleware, getUserById);
+router.get('/results', authMiddleware, getUserResults);
+router.get('/details', authMiddleware, getUserById);
 router.put('/:id', authMiddleware, updateUser);
 router.delete('/:id', authMiddleware, deleteUser); // Admin only
+
 
 module.exports = router;

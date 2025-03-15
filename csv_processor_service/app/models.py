6a84b39b25c9e0995a8b9ab446d5b6fffc64ace3
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-03-15 19:47:06 +0530

    Update models.py

--- csv_processor_service/app/models.py
+++ csv_processor_service/app/models.py
@@ -2,3 +2,9 @@ from pydantic import BaseModel
 
 class FileUploadResponse(BaseModel):
     message: str
+
+
+class PassageResponse(BaseModel):
+    passage: str
+    readability: float | None
+    grade: int

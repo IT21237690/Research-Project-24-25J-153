a0dab7d46cf582fe0530ce1c4d835886587cd6db
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-03-15 15:54:09 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -6,8 +6,9 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['student', 'teacher','admin'], default: 'student' },
+    role: { type: String, enum: ['student', 'teacher', 'admin'], default: 'student' },
     grade: { type: String, required: true },
+    currentDifficulty: { type: Number, required: true, default: 100.0 },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);

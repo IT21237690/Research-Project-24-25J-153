f58247e130830c487df99a0a916dab1a8d2690ca
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-03-18 00:12:10 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -10,6 +10,24 @@ const UserSchema = new mongoose.Schema({
     grade: { type: String, required: true },
     currentDifficulty: { type: Number, required: true, default: 100.0 },
     fluencyDifficulty: { type: Number, required: true, default: 100.0 },
+    interests: [String],
+    watchHistory: [
+        {
+            videoId: String,
+            title: String,
+            watchedAt: {
+                type: Date,
+                default: Date.now,
+            },
+            watchDuration: Number,
+            completed: Boolean,
+        },
+    ],
+    likedVideos: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);

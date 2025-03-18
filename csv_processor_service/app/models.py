e154a4f68d40c086604eeef005a9fa72ec9661e3
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-03-18 23:37:29 +0530

    Update models.py

--- csv_processor_service/app/models.py
+++ csv_processor_service/app/models.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+import datetime
+
 
 class FileUploadResponse(BaseModel):
     message: str
@@ -8,3 +10,9 @@ class PassageResponse(BaseModel):
     passage: str
     readability: float | None
     grade: int
+
+class FileData(BaseModel):
+    file_name: str
+    file: str  # base64 encoded file data
+    userid: str
+    uploaded_date: datetime.datetime

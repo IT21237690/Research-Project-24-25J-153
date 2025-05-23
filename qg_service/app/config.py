d9e3d9fa29d8bdab32290378455358919b82f92f
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-05-24 04:06:57 +0530

    Update config.py

--- qg_service/app/config.py
+++ qg_service/app/config.py
@@ -4,7 +4,7 @@ import torch
 DEVICE = torch.device("cpu")
 
 # Model Path
-MODEL_PATH = "/home/azureuser/custom_qg_model_256v4.pt"
+MODEL_PATH = "/home/azureuser/custom_qg_model_v4.pt"
 
 # Model Name
 MODEL_NAME = "t5-small"

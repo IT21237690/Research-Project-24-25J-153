4d2eaf11759c35a786e2ccc56ac70880e5d95348
Minidu Tissera <99203827+IT21237690@users.noreply.github.com>
2025-05-24 18:53:19 +0530

    Update services.py

--- qa_service/app/services.py
+++ qa_service/app/services.py
@@ -5,15 +5,12 @@ from huggingface_hub import snapshot_download
 MODEL_DIR = "/host_data/QA_Model"
 
 # # Download the model if not already present
-# Download the model if not already present
-if not os.path.exists(MODEL_DIR):
-    os.makedirs(MODEL_DIR)
-    snapshot_download(
-        repo_id="deepset/roberta-base-squad2",  # Replace with the model you want
-        repo_type="model",
-        local_dir=MODEL_DIR,
-        local_dir_use_symlinks=False
-    )
+snapshot_download(
+    repo_id="deepset/roberta-base-squad2",
+    repo_type="model",
+    local_dir=MODEL_DIR,
+    local_dir_use_symlinks=False
+)
 
 # Load the model with CPU enforcement
 qa_pipeline = pipeline("question-answering", model=MODEL_DIR, tokenizer=MODEL_DIR, device=-1)  # Force CPU
